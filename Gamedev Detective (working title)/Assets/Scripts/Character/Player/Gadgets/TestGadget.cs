db0d3eef9c32390e9c2c440ece2b40f37482675e
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2019-10-13 20:41:52 +0100

    a useless member of society

---
+++ Gamedev Detective (working title)/Assets/Scripts/Character/Player/Gadgets/TestGadget.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGadget : Gadget
+{
+    public override void TakeInput() {
+
+        Debug.Log("I'm a TestGadget and I'm a useless member of society");
+        
+    }
+}
