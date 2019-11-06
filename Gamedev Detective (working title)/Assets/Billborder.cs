67797b1bcbd952d645019fdec0788757a03f095c
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2019-11-06 13:53:43 +0000

    rename

---
+++ Gamedev Detective (working title)/Assets/Billborder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billborder : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void LateUpdate() {
+
+      //  transform.rotation = Camera.main.transform.rotation;
+
+         transform.LookAt(Camera.main.transform.position, Vector3.up);
+         
+         
+    }
+}
