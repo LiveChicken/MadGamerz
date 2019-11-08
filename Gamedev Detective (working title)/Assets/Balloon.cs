a310239c8225d06bb89f38b71eaece8a566cb3f1
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2019-11-08 17:32:37 +0000

    Popit

---
+++ Gamedev Detective (working title)/Assets/Balloon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour {
+
+    public float GravityMultiplier;
+
+    private Rigidbody rb;
+
+    void Start() {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    void FixedUpdate() {
+
+        rb.AddForce(Vector3.up * (Physics.gravity.y * GravityMultiplier));
+
+    }
+}
---
+++ Gamedev Detective (working title)/Assets/Popit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class Popit : MonoBehaviour {
+
+     public Transform Target;
+     
+     [SerializeField]
+    private float StaticDistance = 1f;
+     [SerializeField]
+    private float CatchupLerpSpeed = 1f;
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+         if (Vector3.Distance(transform.position, Target.position) > StaticDistance) {
+
+             var MoveDirection =  transform.position - Target.position ;
+              MoveDirection.Normalize();
+              MoveDirection *= StaticDistance;
+
+              transform.position = Vector3.Lerp(transform.position, Target.position + MoveDirection, CatchupLerpSpeed * Time.deltaTime);
+
+
+
+         }
+
+    }
+}
