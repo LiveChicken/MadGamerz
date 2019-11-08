a8957ba04e6ca410e97f28d9db30c46482816701
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2019-11-08 18:30:35 +0000

    volume trigger

---
+++ Gamedev Detective (working title)/Assets/VolumeTrigger.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TFG_Common;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+[RequireComponent(typeof(Collider))]
+public class VolumeTrigger : MonoBehaviour {
+
+
+    public bool OneShotEnter;
+
+    private bool triggered;
+    
+    public LayerMask LayerMask;
+    
+    public UnityEvent EventOnTriggerEnter;
+    public UnityEvent EventOnTriggerStay;
+    public UnityEvent EventOnTriggerExit;
+
+    private Collider trig;
+
+   
+
+
+    private void OnTriggerEnter(Collider other) {
+
+        if (!triggered) {
+
+            if (Checks.CompareLayers(LayerMask, other.gameObject)) {
+
+                EventOnTriggerEnter?.Invoke();
+
+                if (OneShotEnter) {
+
+                    triggered = true;
+
+                }
+
+            }
+        }
+
+
+
+
+    }
+
+    private void OnTriggerStay(Collider other) {
+
+        if (Checks.CompareLayers(LayerMask, other.gameObject)) {
+
+            EventOnTriggerStay?.Invoke();
+
+        }
+        
+        
+    }
+
+    private void OnTriggerExit(Collider other) {
+
+        if (Checks.CompareLayers(LayerMask, other.gameObject)) {
+
+            EventOnTriggerExit?.Invoke();
+
+        }
+        
+    }
+
+    private void OnDrawGizmos() {
+        
+        //Gizmos.DrawWireMesh(trig.bounds);
+        
+    }
+
+
+}
