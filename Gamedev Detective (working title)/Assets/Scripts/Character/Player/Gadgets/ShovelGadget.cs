4d458e4b838e3a8238299d84eb656688bd399405
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2019-11-10 14:48:15 +0000

    I tried

---
+++ Gamedev Detective (working title)/Assets/Scripts/Character/Player/Gadgets/ShovelGadget.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class ShovelGadget : Gadget {
+
+
+    //private bool underground;
+
+    private terrainManager tm;
+
+     public UnityEvent OnFailedToDig;
+     
+     private void OnEnable() {
+
+          tm = transform.GetComponentInParent<terrainManager>();
+
+
+     }
+
+     // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+     void AttemptDig() {
+
+
+          if (tm.isOnDirt) {
+
+               
+               
+               
+               
+          } else {
+
+               
+               OnFailedToDig?.Invoke();
+          }
+
+     }
+
+
+     public override void TakeInputDown() {
+      
+          
+          AttemptDig();
+          
+    }
+
+    public override void TakeInputHold() {
+        
+    }
+
+    public override void TakeInputUp() {
+        
+    }
+     
+     
+     
+}
