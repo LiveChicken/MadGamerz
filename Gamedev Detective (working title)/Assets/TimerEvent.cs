8eb8005d9a055bb3a9e8f5ada84ea575b6330834
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2019-12-05 17:56:30 +0000

    Timer script

---
+++ Gamedev Detective (working title)/Assets/TimerEvent.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class TimerEvent : MonoBehaviour {
+
+    public float TimerDurationDefault = 1f;
+    
+    public UnityEvent OnTimerEnd;
+
+     private bool TimerActive;
+
+     public void StartTimer() {
+
+          StartCoroutine(Timer(TimerDurationDefault));
+
+     }
+
+     public void StartTimer(float duration) {
+
+          StartCoroutine(Timer(duration));
+
+     }
+
+     IEnumerator Timer(float duration) {
+
+          if (TimerActive) {
+            yield break;
+          }
+
+          TimerActive = true;
+          
+          yield return  new WaitForSeconds(duration);
+          
+          OnTimerEnd?.Invoke();
+
+          TimerActive = false;
+
+     }
+
+
+
+
+}
