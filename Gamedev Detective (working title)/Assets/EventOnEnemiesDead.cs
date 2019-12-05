33cf45343e944725ffe8bfdd7baeedce3bec0e03
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2019-12-05 22:41:55 +0000

    ScreenShake Beginnings 2D

--- Gamedev Detective (working title)/Assets/EventOnEnemiesDead.cs
+++ Gamedev Detective (working title)/Assets/EventOnEnemiesDead.cs
@@ -33,6 +33,8 @@ public class EventOnEnemiesDead : MonoBehaviour
                OnAllEnemiesDead?.Invoke();
 
 
+              
+               
           }
 
 
---
+++ Gamedev Detective (working title)/Assets/Shake2D.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+
+public class Shake2D : MonoBehaviour {
+
+    //public CameraShakeInstance CSI;
+
+     public float Magnitude, Roughness, FadeIn, FadeOut;
+     
+     public void Shake() {
+
+          CameraShaker.Instance.ShakeOnce(Magnitude, Roughness, FadeIn, FadeOut);
+     }
+
+}
