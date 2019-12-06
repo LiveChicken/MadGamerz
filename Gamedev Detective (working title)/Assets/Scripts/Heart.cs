257baa89d87c043721c0885cd8e8f6086b431d34
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2019-12-06 22:52:44 +0000

    Health pickup

---
+++ Gamedev Detective (working title)/Assets/Scripts/Heart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+
+
+    
+    public void GivePlayerHealth() {
+
+        try {
+            GameObject player = FindObjectOfType<PlayerMovement2D>().gameObject;
+
+            Debug.Log(player);
+               
+               player.GetComponent<Health>().ChangeHealth(1);
+        } catch {
+
+            Debug.Log("Could not give player health for some reason");
+            
+        }
+    }
+
+}
