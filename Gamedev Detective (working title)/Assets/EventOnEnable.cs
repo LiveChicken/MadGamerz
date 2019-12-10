f0ae6bc8a6d40e462724d7b8e25f72712314618b
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2019-12-10 13:58:27 +0000

    Thankyou screen

---
+++ Gamedev Detective (working title)/Assets/EventOnEnable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.Events;
+
+public class EventOnEnable : MonoBehaviour {
+
+     public UnityEvent OnEnableEvent;
+
+     public UnityEvent OnDisableEvent;
+
+
+     private void OnEnable() {
+          
+          OnEnableEvent?.Invoke();
+          
+     }
+
+     private void OnDisable() {
+          
+          OnDisableEvent?.Invoke();
+          
+     }
+
+}
