913dcf73613c55e35dd0f2a4772b4717497101d9
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2020-02-12 18:41:26 +0000

    Timeline Memory testing

---
+++ Gamedev Detective (working title)/Assets/EndWait.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndWait : MonoBehaviour
+{
+
+     public void TriggerWaitEnd() {
+
+          TimelineManager.endWaitDelegate?.Invoke();
+          
+     }
+
+}
---
+++ Gamedev Detective (working title)/Assets/TimelineManager.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+
+public class TimelineManager : MonoBehaviour {
+    private PlayableDirector PD;
+
+    private bool paused;
+    private bool waiting;
+
+    public delegate void EndWaitDelegate();
+
+    public static EndWaitDelegate endWaitDelegate = delegate {
+
+    };
+
+
+    private void OnEnable() {
+
+         endWaitDelegate += EndWait;
+
+    }
+
+    private void OnDisable() {
+
+         endWaitDelegate -= EndWait;
+
+    }
+
+    private void Start() {
+
+        PD = GetComponent<PlayableDirector>();
+
+    }
+
+
+    private void ContinueTimeline() {
+
+      PD.Play();
+      paused = false;
+
+    }
+
+   public void PauseTimeLine() {
+
+         PD.Pause();
+
+         paused = true;
+
+   }
+
+   public void WaitFor() {
+
+        PauseTimeLine();
+        waiting = true;
+
+   }
+
+   private void EndWait() {
+
+        waiting = false;
+        ContinueTimeline();
+
+
+   }
+
+
+   private void Update() {
+
+        if (paused) {
+
+             if (!waiting) {
+
+                  if (GameManager.GM.controls.Player.A.triggered) {
+
+                       ContinueTimeline();
+
+                  }
+             } 
+
+        }
+
+
+   }
+
+
+}
