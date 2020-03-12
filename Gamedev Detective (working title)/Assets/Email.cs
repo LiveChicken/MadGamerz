98b10489aa0c3e38782ecd0250d1f8964e720446
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2020-03-12 20:41:28 +0000

    email on desk

---
+++ Gamedev Detective (working title)/Assets/Email.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Email : MonoBehaviour
+{
+    public string EmailName;
+
+    private const string Source = "Dialogue/English/";
+
+    private TextAsset TA;
+
+    public TMP_Text Text;
+
+    private void OnEnable()
+    {
+
+        SetText();
+
+    }
+
+    public void SetText()
+    {
+
+        TA = Resources.Load(Source + EmailName) as TextAsset;
+
+        Text.text = TA.text;
+
+
+    }
+
+
+
+
+
+
+
+
+}
