0638dfbb440d39d970aff80d20585b84f4b736ba
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2020-03-29 17:17:20 +0100

    memory art decrypted ish

---
+++ Gamedev Detective (working title)/Assets/Plugins/CameraBoundsViewerOrthographic.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraBoundsViewerOrthographic : MonoBehaviour
+{
+
+    Vector2 size;
+    Vector3 position;
+    Camera Cam;
+    public Color Outline = Color.white;
+     
+
+    private void OnDrawGizmos()
+    {
+#if UNITY_EDITOR
+
+        Cam = GetComponent<Camera>();
+
+        if (Cam.orthographic)
+        {
+
+           size.y=  (Cam.orthographicSize);
+            size.x = (Cam.aspect * Cam.orthographicSize);
+
+            position = Cam.transform.position;
+            position -= new Vector3(size.x, size.y, 0) ;
+
+            size *= 2;
+
+         
+
+        }
+
+
+        Rect rect = new Rect(position, size);
+       
+        UnityEditor.Handles.color = Color.white;
+        
+        UnityEditor.Handles.DrawSolidRectangleWithOutline(rect, Color.clear, Outline);
+
+
+#endif
+
+    }
+
+
+
+}
