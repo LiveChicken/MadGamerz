e00372d936ba53ee9e056322e5c9539c4c444bd0
ConorFT <43806423+ConorFT@users.noreply.github.com>
2020-04-02 18:27:42 +0100

    Moving Anims
    AnimationParameters Script
    -forward
    -left
    -right
    animations in place

---
+++ Gamedev Detective (working title)/Assets/Scripts/Character/Player/AnimationParameters.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationParameters : MonoBehaviour
+{
+    private Controls controls = null;
+
+    private void Awake() => controls = new Controls();
+    private void OnEnable() => controls.Player.Enable();
+    private void OnDisable() => controls.Player.Disable();
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+    
+    void Update()
+    {
+        Move();
+    }
+
+    public float Xvalue;
+
+    private void Move()
+    {
+        var movementInput = controls.Player.Move.ReadValue<Vector2>();
+
+        var movement = new Vector3
+        {
+            x = movementInput.x,
+            z = movementInput.y
+        }.normalized;
+
+        Xvalue = movementInput.y;
+
+        if (movement.z > 0)
+        {
+            anim.SetBool("Moving", true);
+        } else
+        {
+            anim.SetBool("Moving", false);
+        }
+
+        if (movement.x > 0)
+        {
+            anim.SetBool("Right", true);
+        } else
+        {
+            anim.SetBool("Right", false);
+        }
+
+        if (movement.x < 0)
+        {
+            anim.SetBool("Left", true);
+        } else
+        {
+            anim.SetBool("Left", false);
+        }
+    }
+}
